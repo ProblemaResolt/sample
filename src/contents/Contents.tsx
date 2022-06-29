d3e79b84c2be75c94d1da6019dddf8b0bf4ded5e
plorema <boner.engineer@gmail.com>
2022-06-29 10:57:38 +0900

    animation add

--- src/contents/Contents.tsx
+++ src/contents/Contents.tsx
@@ -2,16 +2,61 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 
-const Zoom = require('react-reveal/Zoom')
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 
-export default function FixedContainer() {
+const Zoom = require('react-reveal/Zoom')
+const Slide = require('react-reveal/Slide')
+const Fade  = require('react-reveal/Fade')
+const Roll  = require('react-reveal/Roll')
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+const Contents = () => {
   return (
     <React.Fragment>
+      <Container fixed maxWidth='xl'>
+       <Slide up>
+        <h1> test </h1>
+        <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} >
+        <Grid container spacing={2}>
+         
+        <Grid item xs={8}>
+        <Zoom>
+          <Item>xs=8</Item>
+        </Zoom>
+        </Grid>
+        <Grid item xs={4}>
+        <Roll>
+          <Item>xs=4</Item>
+        </Roll>
+        </Grid>
+        <Grid item xs={4}>
+          <Item>xs=4</Item>
+        </Grid>
+        <Grid item xs={8}>
+          <Item>xs=8</Item>
+        </Grid>
+      </Grid>
+        </Box>
+        </Slide>
       <Zoom>
-      <Container fixed maxWidth='xl' sx={{  margin:'0'}}>
+      <h1> test2 </h1>
         <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} />
-      </Container>
       </Zoom>
+      <Fade left>
+      <h1> test3 </h1>
+        <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} />
+      </Fade >
+      </Container>
     </React.Fragment>
   );
 }
+export default Contents;
